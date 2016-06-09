2bee9af15c697e5ddfd23272d39d5fb28eb03222
bouzuya <m@bouzuya.net>
2016-06-09 23:03:08 +0900

    add getRunner() to helpers/test

--- src/helpers/test.ts
+++ src/helpers/test.ts
@@ -1,13 +1,20 @@
 import { Runner } from '../runner';
 import { ServerRunner } from '../runners';
 
-let runner: Runner;
-const test = <T>(
-  name: string,
-  test: () => T | Promise<T>
-): void => {
-  if (!runner) runner = new ServerRunner(); // Runner is a singleton
+let runner: Runner = null; // singleton
+
+const getRunner = (): Runner => {
+  if (!runner) runner = newRunner();
+  return runner;
+};
+
+const newRunner = (): Runner => {
+  return new ServerRunner();
+};
+
+const test = <T>(name: string, test: () => T | Promise<T>): void => {
+  const runner = getRunner();
   runner.add({ name, test });
 };
 
-export { test };
+export { test, getRunner };
