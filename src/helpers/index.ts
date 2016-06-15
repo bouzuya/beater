4690f6bd118a4584c8055844247df9e868d001ab
bouzuya <m@bouzuya.net>
2016-06-15 20:40:21 +0900

    reduce helpers/index code

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,22 +1,2 @@
-import { process } from '../globals/process';
-import { ChildProcessReporter } from '../child-process-reporter';
-import { FixtureHelper } from '../types/fixture-helper';
-import { Reporter } from '../types/reporter';
-import { TestHelper } from '../types/test-helper';
-import { Runner } from '../runner';
-import { init as initFixture } from './fixture';
-import { init as initTest } from './test';
-
-const init = (reporter?: Reporter): {
-  fixture: FixtureHelper;
-  test: TestHelper;
-} => {
-  const runner = new Runner(
-    typeof process.send === 'undefined' ? reporter : new ChildProcessReporter()
-  );
-  const fixture = initFixture();
-  const test = initTest(runner);
-  return { fixture, test };
-};
-
-export { init };
+export { init as initFixture } from './fixture';
+export { init as initTest } from './test';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-import { init } from './helpers';
+import { process } from './globals/process';
+import { ChildProcessReporter } from './child-process-reporter';
+import { FixtureHelper } from './types/fixture-helper';
+import { Reporter } from './types/reporter';
+import { TestHelper } from './types/test-helper';
+import { initFixture, initTest } from './helpers';
+import { Runner } from './runner';
+
+const init = (reporter?: Reporter): {
+  fixture: FixtureHelper;
+  test: TestHelper;
+} => {
+  const runner = new Runner(
+    typeof process.send === 'undefined' ? reporter : new ChildProcessReporter()
+  );
+  const fixture = initFixture();
+  const test = initTest(runner);
+  return { fixture, test };
+};
+
 export default init;
