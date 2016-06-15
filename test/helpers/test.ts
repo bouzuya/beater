30e87378f6895d1ee55f020a04512f1f76681d7a
bouzuya <m@bouzuya.net>
2016-06-16 08:11:25 +0900

    add helpers/test test

---
+++ test/helpers/test.ts
@@ -0,0 +1,32 @@
+import * as assert from 'power-assert';
+import * as sinon from 'sinon';
+import { test, fixture } from '../helper';
+import { init } from '../../src/helpers/test';
+import { Runner } from '../../src/runner';
+
+type Context = {
+  sandbox: sinon.SinonSandbox;
+  add: sinon.SinonStub;
+  runner: Runner;
+};
+
+const before = (): Context => {
+  const sandbox = sinon.sandbox.create();
+  const add = sandbox.stub();
+  const runner: Runner = <any>{ add };
+  return { sandbox, add, runner };
+};
+
+const after = ({ sandbox }: Context) => sandbox.restore();
+
+test('helpers/test - calls runner.add()', fixture({ before, after }, ({
+  add, runner
+}) => {
+  const t = init(runner);
+  const name = 'name 1';
+  const fn = (): void => void 0;
+  t(name, fn);
+  assert(add.callCount === 1);
+  assert(add.getCall(0).args[0].name === name);
+  assert(add.getCall(0).args[0].fn === fn);
+}));
