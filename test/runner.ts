3674d8dbce92cf6520ff374b11df66805864ad00
bouzuya <m@bouzuya.net>
2016-06-16 14:06:36 +0900

    add dummy test for runner

---
+++ test/runner.ts
@@ -0,0 +1,8 @@
+import * as assert from 'power-assert';
+import { test, fixture } from './helper';
+import { Runner } from '../src/runner';
+
+// TODO:
+test('runner (TODO)', () => {
+  assert(Runner);
+});
